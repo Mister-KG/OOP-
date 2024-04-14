2298e60c58ad8dbfee3bd923faa2283b0adda032
Kagiso Kgobane <117633929+Mister-KG@users.noreply.github.com>
2024-04-14 16:41:12 +0200

    Add files via upload

---
+++ Encapsulation/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Person myObj = new Person();
+        myObj.setName("John");
+        System.out.println(myObj.getName());
+    }
+}
---
+++ Inheritance/src/main/java/org/example/Vehicle.java
@@ -0,0 +1,8 @@
+package org.example;
+
+ class Vehicle {
+     protected String brand = "Ford";
+     public void honk(){
+         System.out.println("beep,beep");
+     }
+}
